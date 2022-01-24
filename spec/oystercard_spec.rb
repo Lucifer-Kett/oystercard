67df3d25c32f7230acefa4d3d5d10ac7e3dc8752
Luke Collins <lukecollins@Lukes-MacBook-Air.local>
2022-01-24 16:41:45 +0000

    Chap4: Fixed balance setting when init

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,9 @@ require 'oystercard.rb'
 
 describe Oystercard do
     card = Oystercard.new
+
     it 'can begin with a balance' do
-    expect(card = 30).to eq(30)
+     card = Oystercard.new(30)
+    expect(card.balance).to eq(30)
     end
 end
