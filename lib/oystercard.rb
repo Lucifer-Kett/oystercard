d155332b0298403ddc8002d5a0b38d872be99e57
Luke Collins <lukecollins@Lukes-MacBook-Air.local>
2022-01-24 16:48:57 +0000

    Chap5: Top Up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,4 +4,8 @@ attr_reader :balance
    def initialize(balance = 0)
     @balance = balance
    end
+
+   def top_up(add)
+    @balance += add
+   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,7 +4,12 @@ describe Oystercard do
     card = Oystercard.new
 
     it 'can begin with a balance' do
-     card = Oystercard.new(30)
-    expect(card.balance).to eq(30)
+     card1 = Oystercard.new(30)
+    expect(card1.balance).to eq(30)
     end
+
+    it 'can be topped up' do
+    expect(card.top_up(20)).to eq(20)
+    end
+
 end
